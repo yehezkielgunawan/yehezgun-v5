5c1b8616005d3b6777cba2d24d5179173976fcc8
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-18 15:20:58 +0700

    feat(resume.tsx): :sparkles: resume page

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,11 @@
+const ResumePage = () => {
+	return (
+		<iframe
+			title="Resume"
+			src="https://drive.google.com/file/d/1oXe3GJlPHHDE3w14IzLSa650jXgvG-_K/preview"
+			className="h-screen w-full"
+		/>
+	);
+};
+
+export default ResumePage;
