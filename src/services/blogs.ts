05fc09da09a89ed51d4c163dadc55c4e8ed8f3d4
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-19 15:02:08 +0700

    feat(services/blogs): :sparkles: setup blog list data
    so far dummy

---
+++ src/services/blogs.ts
@@ -0,0 +1,18 @@
+import { allBlogs } from "content-collections";
+
+export const blogList = allBlogs.map((post) => ({
+	title: post.title,
+	summary: post.summary,
+	coverImg: post.coverImg,
+	date: post.date,
+	category: post.category,
+	slug: post._meta.path,
+}));
+
+export const getBlogBySlug = (slug: string) => {
+	const post = allBlogs.find((blog) => blog._meta.path === slug);
+	if (!post) {
+		throw new Error(`No post found for slug: ${slug}`);
+	}
+	return post;
+};
