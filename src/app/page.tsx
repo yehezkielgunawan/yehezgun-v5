313cc55e64a89d2d6d00592a96ded6de4e8e8072
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-19 11:05:32 +0700

    fix(home): :bug: sort featured page based on date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,34 +49,36 @@ export default function Home() {
 			<section className="mt-12">
 				<h2>Featured Projects</h2>
 				<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-					{featuredProjects.map((project) => (
-						<div key={project.name} className="relative">
-							<a
-								href={project.url}
-								target="_blank"
-								className="group"
-								rel="noreferrer"
-							>
-								<figure className="h-52 w-full">
-									<Image
-										src={project.projectHero}
-										width={320}
-										height={320}
-										alt={project.name}
-										className="h-full w-full rounded-lg object-cover blur-none brightness-90 transition duration-300 hover:outline hover:outline-secondary hover:blur-none md:blur-[2px]"
-									/>
+					{featuredProjects
+						.sort(
+							(a, b) =>
+								(new Date(b.date) as unknown as number) -
+								(new Date(a.date) as unknown as number),
+						)
+						.map((project) => (
+							<div key={project.name} className="relative">
+								<a
+									href={project.url}
+									target="_blank"
+									className="group"
+									rel="noreferrer"
+								>
+									<figure className="h-52 w-full">
+										<Image
+											src={project.projectHero}
+											width={320}
+											height={320}
+											alt={project.name}
+											className="h-full w-full rounded-lg object-cover blur-none brightness-90 transition duration-300 hover:outline hover:outline-secondary hover:blur-none md:blur-[2px]"
+										/>
 
-									<figcaption className="-translate-x-1/2 absolute bottom-0 left-1/2 w-full text-center font-bold text-base-content">
-										{project.name}
-									</figcaption>
-								</figure>
-							</a>
-							{/* <div className="card-body pt-0">
-								<h3>{project.name}</h3>
-								<p>{project.description}</p>
-							</div> */}
-						</div>
-					))}
+										<figcaption className="-translate-x-1/2 absolute bottom-0 left-1/2 w-full text-center font-bold text-base-content">
+											{project.name}
+										</figcaption>
+									</figure>
+								</a>
+							</div>
+						))}
 				</div>
 			</section>
 		</GeneralWrapper>
