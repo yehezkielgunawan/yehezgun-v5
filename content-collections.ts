46bc43f33976dc5649189c7e1a4a7e6cb5169695
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-19 16:53:02 +0700

    feat(content-collections.ts): :wrench: adjust config for mdx

--- content-collections.ts
+++ content-collections.ts
@@ -1,5 +1,10 @@
 import { defineCollection, defineConfig } from "@content-collections/core";
 import { compileMDX } from "@content-collections/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeRaw from "rehype-raw";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import remarkHtml from "remark-html";
 const projects = defineCollection({
 	name: "projects",
 	directory: "content/projects",
@@ -27,7 +32,10 @@ const blogs = defineCollection({
 		category: z.string(),
 	}),
 	transform: async (document, context) => {
-		const mdx = await compileMDX(context, document);
+		const mdx = await compileMDX(context, document, {
+			rehypePlugins: [rehypeAutolinkHeadings, rehypeSlug, rehypeRaw],
+			remarkPlugins: [remarkGfm, remarkHtml],
+		});
 		return {
 			...document,
 			mdx,
