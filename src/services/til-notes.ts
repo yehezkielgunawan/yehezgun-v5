c06d92817cafa08b9afacc1ba44f37ed198807bb
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-29 10:37:35 +0700

    feat(til-notes.ts): :sparkles: function to get til note by slug

--- src/services/til-notes.ts
+++ src/services/til-notes.ts
@@ -7,3 +7,11 @@ export const allTILNotes = allTilNotes.map((note) => ({
 	tags: note.tags,
 	slug: note._meta.path,
 }));
+
+export const getTILNoteBySlug = (slug: string) => {
+	const note = allTilNotes.find((note) => note._meta.path === slug);
+	if (!note) {
+		throw new Error(`No note found for slug: ${slug}`);
+	}
+	return note;
+};
