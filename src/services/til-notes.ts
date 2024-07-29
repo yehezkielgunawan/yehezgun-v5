9bb0482ffaf7727fe180d455997b0da45e00b74f
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-29 10:19:36 +0700

    refactor(til-notes.ts): :recycle: redefine the data type

--- src/services/til-notes.ts
+++ src/services/til-notes.ts
@@ -1,3 +1,9 @@
 import { allTilNotes } from "content-collections";
 
-export const allTILNotes = allTilNotes;
+export const allTILNotes = allTilNotes.map((note) => ({
+	title: note.title,
+	subtitle: note.subtitle,
+	date: note.date,
+	tags: note.tags,
+	slug: note._meta.path,
+}));
