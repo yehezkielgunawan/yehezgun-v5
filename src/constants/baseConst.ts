cf19048e968034e6e7f1a4bbf2ca7da193c7590d
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-30 17:47:36 +0700

    feat(baseConst.ts): :sparkles: add general base const var

---
+++ src/constants/baseConst.ts
@@ -0,0 +1,3 @@
+export const repoName = process.env.publicRepo as `${string}/${string}`;
+export const repoId = process.env.repoId as string;
+export const categoryId = process.env.categoryId as string;
