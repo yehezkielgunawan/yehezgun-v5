b01a3a6964a9921386c15eae18a4944372a4a6a8
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-30 10:41:10 +0700

    fix(QuickNotesCard.tsx): :bug: change til route to quick-notes

--- src/components/QuickNotesCard.tsx
+++ src/components/QuickNotesCard.tsx
@@ -10,7 +10,7 @@ type QuickNotesProps = {
 const QuickNotesCard = (til: QuickNotesProps) => {
 	return (
 		<Link
-			href={`/til/${til.slug}`}
+			href={`/quick-notes/${til.slug}`}
 			className="group hover:-translate-y-0.5 rounded-lg bg-base-300 no-underline transition duration-300 hover:underline hover:shadow-md dark:hover:shadow-neutral-content"
 		>
 			<div className="flex h-full w-full flex-col items-start justify-between rounded-lg p-2">
