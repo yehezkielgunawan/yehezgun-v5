e13c071f016ffd2ff2454c7e1140f04fd4edc144
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-30 11:06:11 +0700

    refactor(formatDate.ts): :recycle: make it more reusable

--- src/services/formatDate.ts
+++ src/services/formatDate.ts
@@ -1,8 +1,15 @@
-export const formatDate = (date: string) => {
+export const formatDate = (
+	date: string,
+	dateFormat?: {
+		year?: "numeric" | "2-digit";
+		month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
+		day?: "numeric" | "2-digit";
+	},
+) => {
 	const options: Intl.DateTimeFormatOptions = {
-		year: "numeric",
-		month: "short",
-		day: "numeric",
+		year: dateFormat?.year || "numeric",
+		month: dateFormat?.month || "short",
+		day: dateFormat?.day || "numeric",
 	};
 	return new Date(date).toLocaleDateString("en-US", options);
 };
