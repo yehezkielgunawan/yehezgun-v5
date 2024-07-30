77424f745b35ad87c29373b8e3df7cf32b148a41
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-30 12:30:19 +0700

    fix(formatDate.ts): :bug: fix function args

--- src/services/formatDate.ts
+++ src/services/formatDate.ts
@@ -1,15 +1,15 @@
 export const formatDate = (
 	date: string,
-	dateFormat?: {
+	dateFormat: {
 		year?: "numeric" | "2-digit";
 		month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
 		day?: "numeric" | "2-digit";
-	},
+	} = {},
 ) => {
 	const options: Intl.DateTimeFormatOptions = {
-		year: dateFormat?.year || "numeric",
-		month: dateFormat?.month || "short",
-		day: dateFormat?.day || "numeric",
+		year: dateFormat.year || "numeric",
+		month: dateFormat.month || "short",
+		day: dateFormat.day,
 	};
 	return new Date(date).toLocaleDateString("en-US", options);
 };
