7ea91a6aaa569627f302ce6d3d36bf9b4aac06b1
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-30 10:40:03 +0700

    refactor(menuList.ts): :recycle: change til to quick notes

--- src/app/quick-notes/page.tsx
+++ src/app/quick-notes/page.tsx
@@ -5,14 +5,14 @@ import { quickNotesList } from "@/services/quick-notes";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Today I Learned",
+	title: "Quick Notes",
 	description: "A quick collection of things I learn on a daily basis.",
 };
 const QuickNotesPage = () => {
 	return (
 		<GeneralWrapper>
 			<section>
-				<h1>Today I Learned</h1>
+				<h1>Quick Notes</h1>
 				<p>A Quick Notes From What I've Figured Out.</p>
 			</section>
 			<section className="mt-12">
