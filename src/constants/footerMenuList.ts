30f76026d41896eab55cd1987235e7679a5647aa
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-08-02 12:42:04 +0700

    feat(Footer.tsx): :sparkles: add footer nav links

---
+++ src/constants/footerMenuList.ts
@@ -0,0 +1,14 @@
+export const footerMenuList = [
+	{
+		title: "Personal Docs",
+		url: "https://docs.yehezgun.com",
+	},
+	{
+		title: "Templates",
+		url: "https://github.com/yehezkielgunawan?tab=repositories&q=template&type=&language=&sort=",
+	},
+	{
+		title: "Analytics",
+		url: "https://umami-v3.yehezgun.com/share/yaPWD5zDY4XPFNTC/yehezgun.com",
+	},
+];
