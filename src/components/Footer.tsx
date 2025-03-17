51f7a5b9a2421eef719f300bd21dc20ab63de691
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-03-17 20:43:12 +0700

    refactor(footer): restructure layout for improved responsiveness and alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,31 @@ import Link from "next/link";
 
 const Footer = () => {
 	return (
-		<footer className="footer footer-center container mx-auto mt-12 mb-16 gap-6 text-base-content lg:max-w-5xl">
+		<footer className="footer container mx-auto mt-12 mb-16 gap-6 text-base-content lg:max-w-5xl">
 			<hr className="w-full border-[0.5px] border-neutral-content" />
-			<nav className="flex flex-wrap items-center gap-4 md:gap-8">
-				{footerMenuList.map((menu) => (
-					<Link
-						key={menu.title}
-						href={menu.url}
-						target="_blank"
-						className="link link-hover font-semibold"
-						rel="noreferrer"
-					>
-						{menu.title}
-					</Link>
-				))}
-			</nav>
-			<a
-				href="https://yehezgun.com"
-				target="_blank"
-				className="link link-hover"
-				rel="noreferrer"
-			>
-				© {new Date().getFullYear()} | Yehezgun
-			</a>
+			<div className="flex w-full flex-wrap items-center justify-center gap-4 md:justify-between">
+				<nav className="flex flex-wrap items-center gap-4 md:gap-8">
+					{footerMenuList.map((menu) => (
+						<Link
+							key={menu.title}
+							href={menu.url}
+							target="_blank"
+							className="link link-hover font-semibold"
+							rel="noreferrer"
+						>
+							{menu.title}
+						</Link>
+					))}
+				</nav>
+				<a
+					href="https://yehezgun.com"
+					target="_blank"
+					className="link link-hover"
+					rel="noreferrer"
+				>
+					© {new Date().getFullYear()} | Yehezgun
+				</a>
+			</div>
 		</footer>
 	);
 };
