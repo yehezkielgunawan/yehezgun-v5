ec24863ca3b8b79910dd0fcad78a7b725e828024
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-05-12 10:04:45 +0700

    chore: update vitest configuration to include globals and setup files

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,11 @@ export default defineConfig({
 	test: {
 		include: ["src/__test__/*.test.ts", "src/__test__/*.test.tsx"],
 		environment: "jsdom",
+		globals: true,
+		setupFiles: ["./src/__test__/setup.ts"],
 		alias: {
 			"@": "/src",
+			"content-collections": "/src/__test__/mocks/content-collections.ts",
 		},
 	},
 });
