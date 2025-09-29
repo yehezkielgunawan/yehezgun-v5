d09370e820a2de46cf93d249a7b24b71b316e3ee
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-09-29 20:14:05 +0700

    refactor: remove Analytics link from footer menu list

--- src/constants/footerMenuList.ts
+++ src/constants/footerMenuList.ts
@@ -1,14 +1,10 @@
 export const footerMenuList = [
-	{
-		title: "Personal Docs",
-		url: "https://docs.yehezgun.com",
-	},
-	{
-		title: "Templates",
-		url: "https://github.com/yehezkielgunawan?tab=repositories&q=template&type=&language=&sort=",
-	},
-	{
-		title: "Analytics",
-		url: "https://umami-2.yehezgun.com/share/6D9Snfxa1vGbxpIj/yehezgun.com",
-	},
+  {
+    title: "Personal Docs",
+    url: "https://docs.yehezgun.com",
+  },
+  {
+    title: "Templates",
+    url: "https://github.com/yehezkielgunawan?tab=repositories&q=template&type=&language=&sort=",
+  },
 ];
