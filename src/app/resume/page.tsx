27d14d85e0d0aa1a694dc5bd11449940747e4b2a
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2026-02-17 15:10:01 +0700

    Update resume iframe source URL and adjust dimensions

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,12 @@
 const ResumePage = () => {
 	return (
 		<iframe
+			src="https://drive.google.com/file/d/1qfmpSD-dMIxj78yaQBaRgQQpIPSzJ_y7/preview"
+			width="640"
+			height="480"
 			title="Resume"
-			src="https://drive.google.com/file/d/1oXe3GJlPHHDE3w14IzLSa650jXgvG-_K/preview"
 			className="h-screen w-full"
-		/>
+		></iframe>
 	);
 };
 
